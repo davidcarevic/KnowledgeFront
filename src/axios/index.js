5ae46e47d104d463ec0c9e3183f1896de0498bf4
DavidKroon <david@kroonstudio.com>
2020-02-12 16:06:30 +0100

    Refresh interceptor

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import store from '../redux/configureStore';
+import {getRefresh} from '../services';
 
 const instance = axios.create({
     baseURL: process.env.BASE_URL || 'http://localhost:8000/',
@@ -10,6 +11,18 @@ instance.interceptors.request.use(config => {
         config.headers.common.Authorization = `Bearer ${store.getState().user.accessToken}`
     }
     return config;
-});
-
+},);
+instance.interceptors.response.use( response => {
+    return response
+},error=>{
+    axios.interceptors.response.eject();
+    getRefresh()
+        .then(res => {
+            instance.interceptors.request.use(config => {
+                    config.headers.common.Authorization = `Bearer ${res.data.access}`
+                return config;
+            });
+        })
+        .catch(err=>console.log(err.message))
+})
 export default instance;
