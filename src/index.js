cbbfcde963ba374c629e742d42fc932a9da83246
JovanKroon <jovan@kroonstudio.com>
2020-02-19 10:42:22 +0100

    Theme provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import  {Provider}  from 'react-redux';
+import { ThemeProvider } from 'styled-components'
 import store from './redux/configureStore';
-
 import './index.css';
 import App from './App';
-
+import theme from './theme'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
     </Provider>
     , document.getElementById('root'));
 
