458016588ea793c58e5f22e8907e05a0b0590f63
DavidKroon <david@kroonstudio.com>
2020-02-21 16:47:26 +0100

    Forgot pass and reset

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import CreateProject from "./containers/CreateProject";
 import InviteUser from "./containers/InviteUser";
 import Wrapper from "./components/blocks/Wrapper";
 import ForgotPassword from "./containers/ForgotPassword";
+import SingleProject from "./containers/SingleProject";
 
 const App = props => (
   <Wrapper>
@@ -27,11 +28,13 @@ const App = props => (
       <Route exact path="/" component={Home} />
       <Route exact path="/register" component={Register}/>
       <Route exact path="/register/:guid" component={Register}/>
-      <Route exact path="/forgotpassword" component={ForgotPassword}/>
+      <Route exact path="/forgot-password" component={ForgotPassword}/>
+      <Route exact path="forgot-password/:guid" component={ForgotPassword}/>
       <PrivateRoute exact path="/dashboard" Component={Dashboard}/>
       <PrivateRoute exact path="/dashboard/teams/create" Component={CreateTeam}/>
       <PrivateRoute exact path="/dashboard/:id" Component={TeamProjects}/>
       <PrivateRoute exact path="/dashboard/projects/create" Component={CreateProject}/>
+      <PrivateRoute exact path="/dashboard/projects/:id" Component={SingleProject}/>
       <PrivateRoute exact path="/invite" Component={InviteUser}/>
       <Route exact path="/testnav" component={TestNav}/>
       </Switch>
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 import axios from '../axios';
 
 export const getToken = (email, password) => {
-    console.log("asdasdasds")
       return axios.post('/api/token/', {
           email,
           password
@@ -13,23 +12,16 @@ export const removeToken = () => {
 }
 
 export const getRefresh = () =>{
-    return axios.post('/api/token/refresh/',{
-        refresh:window.localStorage.getItem("refreshToken")
+    return axios.post('/api/token/refresh/', {
+        refresh: window.localStorage.getItem("refreshToken")
     })
 }
 
-<<<<<<< HEAD
 export const register = (email, password, data) => {
   return axios.post('/api/users/', {
-     email: email, 
+     email: email,
      password: password,
      data: data
-=======
-export const register = (email, password,data) => {
-  console.log("usao u reg")
-  return axios.post('/api/users/',{
-     email:email, password:password,data:data
->>>>>>> 776c97621e1c7ee7c6a5fdaa21d04de73746b458
   })
 }
 
@@ -38,22 +30,26 @@ export const getTeamsByUser = () => {
 }
 
 export const createTeam = (name, description) => {
-    return axios.post('/api/teams/',{
+    return axios.post('/api/teams/', {
         name: name,
         description: description,
-        })
+    })
 }
 
 export const getProjects = (id) => {
-    return axios.get('/api/teams/'+id+'/')
+    return axios.get('/api/teams/' + id + '/')
 }
 
 export const getProjectsByUser = () => {
     return axios.get('/api/user-projects/')
 }
 
+export const getSingleProject = (id) => {
+    return axios.get('/api/projects/' + id + '/')
+}
+
 export const createProject = (name, description, teamID) => {
-    return axios.post('/api/projects/',{
+    return axios.post('/api/projects/', {
         name: name,
         description: description,
         teamID: teamID
@@ -61,7 +57,7 @@ export const createProject = (name, description, teamID) => {
 }
 
 export const inviteUser = (email, data) => {
-    return axios.post('/api/email-invitations/',{
+    return axios.post('/api/email-invitations/', {
         email: email,
         data: data
     })
@@ -72,12 +68,23 @@ export const getInvitedUser = (guid) => {
 }
 
 export const resetPass = (email) => {
-    return true
-<<<<<<< HEAD
+    return axios.post('/api/password-reset/', {
+        email:email
+    })
+}
+
+export const getResetPassForUser = (guid) => {
+    return axios.get('/api/password-reset/'+ guid + '/')
+}
+
+export const destroyResetAndUpdatePass = (guid, password, data) => {
+    return axios.put('/api/password-reset/'+ guid +'/',{
+        guid: guid,
+        password: password,
+        data: data
+    })
 }
 
 export const getProjectSections = (id) => {
     return axios.get('/api/project-sections/' + id + '/')
-=======
->>>>>>> 776c97621e1c7ee7c6a5fdaa21d04de73746b458
 }
