00b6583aaf3f138b60e76af00e62f76518cf4d5d
JovanKroon <jovan@kroonstudio.com>
2020-02-27 16:49:18 +0100

    styling

--- src/containers/DragAndDrop/styled/index.js
+++ src/containers/DragAndDrop/styled/index.js
@@ -3,18 +3,18 @@ const grid = 8
 export const getItemStyle = (isDragging, draggableStyle) => ({
     // some basic styles to make the items look a bit nicer
     userSelect: 'none',
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
+    padding: '5px',
+    margin: `0 0 5px 0`,
 
     // change background colour if dragging
-    background: isDragging ? 'lightgreen' : 'grey',
+    background: isDragging ? 'lightgreen' : 'papayawhip',
 
     // styles we need to apply on draggables
     ...draggableStyle
 });
 
 export const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? 'lightblue' : 'lightgrey',
-    padding: grid,
+    background: isDraggingOver ? 'lightblue' : 'papayawhip',
+    padding: 0,
     width: 250
 });
