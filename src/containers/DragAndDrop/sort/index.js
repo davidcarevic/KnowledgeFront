630cd2fe473f12fb920d6db7d9a9afc358e279de
DavidKroon <david@kroonstudio.com>
2020-03-04 14:26:03 +0100

    Sort function finalized

--- src/containers/DragAndDrop/sort/index.js
+++ src/containers/DragAndDrop/sort/index.js
@@ -8,49 +8,43 @@ const sortElements=props=> {
     for(let i = 0 ; i <  props.length; i++){
         if(props[i].order.length>0) {
             order[props[i].id] = props[i].order;
-
-            // for(let j = 0; j < props[i].order.length; j++){
-            //     console.log("trenutni order", props[i].order[i])
-            //     if(props[i][props[i].id]===props[i].order[i]){
-            //         order[props[i].id] = props[i][props[i].id];
-            //     }
-            // }
         }
         else{
             order[props[i].id] = props[i][props[i].id];
         }
-
         new_state[props[i].id] = props[i][props[i].id]
-
     }
 
-
     /** remapping of the new state to fit the order*/
+    /** tmp adds the unsorted elements to the ordered array*/
     for(let category = 1; category < new_state.length; category++){
         new_state[category].forEach((ele,index)=>{
             let i = 0;
-            for(i ; i < order[category].length; i++) {
+            for(i; i < order[category].length; i++) {
                 if (ele.id === order[category][i]) {
                     order[category][i]=ele
                 }
                 else{
-                    tmp[index]=ele;
+                    tmp[index] = ele;
+                }
+            }
+            for(i; i < tmp.length; i++){
+                if(ele.id===tmp[i].id){
+                    order[category][index]=tmp[i]
                 }
             }
         })
     }
-
-    console.log("NEW STATE ", new_state);
-    console.log("ORDER POSLE" , order);
-    console.log("TMP ", tmp);
-
+    // console.log("NEW STATE ", new_state);
+    // console.log("ORDER POSLE" , order);
+    // console.log("TMP ", tmp);
     /** converts the id of the element into string*/
-    for(let p in new_state) {
-        for(let i = 0; i < new_state[p].length; i++) {
-            new_state[p][i].id = new_state[p][i].id.toString()
+    for(let p in order) {
+        for(let i = 0; i < order[p].length; i++) {
+            order[p][i].id = order[p][i].id.toString()
         }
     }
 
-    return new_state
+    return order
 };
 export default sortElements
