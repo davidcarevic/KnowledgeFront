2084abb4d710f0e444141c55f0a0f33a8068e823
DavidKroon <david@kroonstudio.com>
2020-03-12 23:58:51 +0100

    Popover ID display fix.

--- src/components/itemTypes/Popover/index.js
+++ src/components/itemTypes/Popover/index.js
@@ -2,39 +2,23 @@ import React, { Component } from 'react';
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
 import StyledLink from "../../elements/Link";
 
-class Popover extends Component {
-  constructor(props) {
-    super(props)
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      popoverOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      popoverOpen: !this.state.popoverOpen
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Button id="PopoverFocus" type="button">
-          Add new item
+const Popover=(props)=>(
+    <div>
+        <div>{props.element}</div>
+        <Button key={props.element} id={"PopoverFocus"+props.element} type="button">
+            Add new item
         </Button>
-        <UncontrolledPopover onClick={this.toggle} placement="right" triger="focus" target="PopoverFocus" isOpen={this.state.popoverOpen} toggle={this.toggle}>
-          <PopoverHeader>Select item type</PopoverHeader>
-          <PopoverBody>
-              <StyledLink to={"/dashboard/projects/" + this.props.project + "/section/" + this.props.section + "/category/" + this.props.category + "/element/" +
-                  this.props.element + "/item-create/embed"}>Iframe</StyledLink><br/>
-              <StyledLink to={"/dashboard/projects/" + this.props.project + "/section/" + this.props.section + "/category/" + this.props.category + "/element/" +
-                  this.props.element + "/item-create/richText"}>Rich Text</StyledLink>
-          </PopoverBody>
+        <UncontrolledPopover  placement="right" triger="focus" target={"PopoverFocus"+props.element}>
+            <PopoverHeader>Select item type</PopoverHeader>
+            <PopoverBody>
+                <StyledLink
+                    to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
+                    props.element + "/item-create/embed"}>Iframe</StyledLink><br/>
+                <StyledLink
+                    to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
+                    props.element+ "/item-create/richText"}>Rich Text</StyledLink>
+            </PopoverBody>
         </UncontrolledPopover>
-      </div>
-    );
-  }
-}
-
+    </div>
+)
 export default Popover
