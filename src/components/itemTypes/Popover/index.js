d09956b2c3dec5783c27e6bbce0d486b8b3d57f8
DavidKroon <david@kroonstudio.com>
2020-03-17 12:42:08 +0100

    Popover Style

--- src/components/itemTypes/Popover/index.js
+++ src/components/itemTypes/Popover/index.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
 import StyledLink from "../../elements/Link";
+import {ButtonS, UncontrolledPopoverStyled, BodyStyled,HeaderStyled} from "./styled"
 
-const Popover=(props)=>(
-    <div>
-        <Button key={props.element} id={"PopoverFocus"+props.element} type="button">
-            Add new item
-        </Button>
-        <UncontrolledPopover  placement="right" triger="focus" target={"PopoverFocus"+props.element}>
-            <PopoverHeader>Select item type</PopoverHeader>
-            <PopoverBody>
-                <StyledLink
-                    to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
-                    props.element + "/item-create/embed"}>Iframe</StyledLink><br/>
-                <StyledLink
-                    to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
-                    props.element+ "/item-create/richText"}>Rich Text</StyledLink>
-            </PopoverBody>
-        </UncontrolledPopover>
-    </div>
-)
+
+const Popover=(props)=> {
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const toggle = () => setPopoverOpen(!popoverOpen);
+    return (
+        <div>
+            <ButtonS key={props.element} id={"PopoverFocus" + props.element} type="button" onClick={toggle}>
+                Add new item
+            </ButtonS>
+            <UncontrolledPopoverStyled placement="right" triger="focus" target={"PopoverFocus" + props.element}
+                                 isOpen={popoverOpen} toggle={toggle}>
+                <HeaderStyled>Select item type</HeaderStyled>
+                <BodyStyled>
+                    <StyledLink
+                        to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
+                        props.element + "/item-create/embed"}>Iframe</StyledLink><br/>
+                    <StyledLink
+                        to={"/dashboard/projects/" + props.project + "/section/" + props.section + "/category/" + props.category + "/element/" +
+                        props.element + "/item-create/richText"}>Rich Text</StyledLink>
+                </BodyStyled>
+            </UncontrolledPopoverStyled>
+        </div>
+    )
+}
 
 export default Popover
