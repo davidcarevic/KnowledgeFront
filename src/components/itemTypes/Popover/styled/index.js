3f1555593e46fc4fb87ba42ca1cb797c476c88c0
DavidKroon <david@kroonstudio.com>
2020-03-17 13:15:26 +0100

    Popover

---
+++ src/components/itemTypes/Popover/styled/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
+
+
+export const ButtonS = styled(Button)`
+  /* Adapt the colors based on primary prop */
+  background-color: black;
+  color: white;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border-radius: 5px;
+  width: 315px;
+  height: 35px;
+`;
+
+export const UncontrolledPopoverStyled = styled(UncontrolledPopover)`
+    position: relative;
+`;
+
+export const BodyStyled = styled(PopoverBody)`
+    padding: 5px;
+    background-color:white;
+`;
+
+export const HeaderStyled = styled(PopoverHeader)`
+    padding: 5px;
+    background-color:white
+`;
